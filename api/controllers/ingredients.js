e20fcdf4feafadfb9d2dd134d48fcaced3af9b27
Randall Spencer <randallspencer@gmail.com>
2017-03-21 13:07:50 -0700

    Pass POST /ingredients test

--- api/controllers/ingredients.js
+++ api/controllers/ingredients.js
@@ -7,35 +7,6 @@ module.exports = {
     getIngredientsList: getIngredientsList
 };
 
-// function getIngredientsList(req, res) {
-//     // To list ingredients
-//     return knex("ingredients").select("id", "name").then((rows) => {
-//         let result = {
-//             ingredients: rows
-//         };
-//         console.log('RESULT', result);
-//         delete result.created_at;
-//         delete result.created_at;
-//         return res.json(result);
-//     });
-// }
-
-// function getIngredientsList(req, res) {
-//     // To list ingredients
-//     return knex("ingredients")
-//         .join('ingredient_tags', 'ingredients.id', 'ingredient_tags.ingredient_id')
-//         .select('*')
-//         .then((rows) => {
-//             let result = {
-//                 ingredients: rows
-//             };
-//             console.log('RESULT', result);
-//             delete result.created_at;
-//             delete result.created_at;
-//             return res.json(result);
-//         });
-// }
-
 function getIngredientsList(req, res) {
 
     // To list ingredients
@@ -56,8 +27,6 @@ function getIngredientsList(req, res) {
                 }).sort();
 
                 ingredient.tags = t;
-
-                console.log(ingredient);
             };
 
             let payload = {
@@ -83,19 +52,89 @@ function getIngredient(req, res) {
             let ingredient = results[0];
             let tags = results[1];
 
-            let t = tags.map((tag) => {
-                return tag.tag_text;
-            }).sort();
+            if (ingredient === undefined) {
+                res.status(400).json('Ingredient not found');
+            } else {
+                let t = tags.map((tag) => {
+                    return tag.tag_text;
+                }).sort();
 
-            ingredient.tags = t;
-            ingredient.alternatives = [];
+                ingredient.tags = t;
+                ingredient.alternatives = [];
 
-            delete ingredient.created_at;
+                delete ingredient.created_at;
 
-            return res.json(ingredient);
+                return res.json(ingredient);
+            }
         })
 }
 
+// function queryIngredient(id) {
+//     let promises = [];
+//     promises.push(
+//         knex("ingredients").select("id", "name")
+//         .first().where("id", id)
+//     );
+//     promises.push(
+//         knex("ingredient_tags").select("ingredient_id", "tag_text")
+//         .where("ingredient_id", id)
+//     );
+//     Promise.all(promises)
+//         .then((results) => {
+//             let ingredient = results[0];
+//             let tags = results[1];
+//
+//             if (ingredient === undefined) {
+//                 throw new Error();
+//             } else {
+//                 let t = tags.map((tag) => {
+//                     return tag.tag_text;
+//                 }).sort();
+//
+//                 ingredient.tags = t;
+//                 ingredient.alternatives = [];
+//                 console.log("id", id);
+//                 console.log(ingredient);
+//                 return ingredient;
+//             }
+//         })
+//         .catch((err) => {
+//           throw new Error();
+//         });
+// };
+
 function addIngredient(req, res) {
-    return res.json({});
+    let name = req.swagger.params.ingredient.value.name;
+    var id;
+    knex("ingredients")
+        .first().where("name", name)
+        .then((result) => {
+            if (result) {
+                res.status(400).json('Ingredient already exists!');
+            } else {
+                return knex("ingredients").insert({
+                    "name": name
+                }).returning('*');
+            }
+        })
+        .then((ingredient) => {
+            if (req.swagger.params.ingredient.value.tags) {
+                let promises = [];
+                for (let val of req.swagger.params.ingredient.value.tags) {
+                    promises.push(
+                        knex("ingredient_tags").insert({
+                            "ingredient_id": ingredient[0].id,
+                            "tag_test": val
+                        }).returning('*')
+                    );
+                }
+                return ingredient;
+            }
+        })
+        .then((ingredient) => {
+            // return res.json(queryIngredient(ingredient[0].id));
+            let newIngredient = req.swagger.params.ingredient.value;
+            newIngredient.id = ingredient[0].id;
+            return res.json(newIngredient);
+        });
 }
