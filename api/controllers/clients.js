19278e0fbccc7d94a68c5a8d861236a43139ff3a
Randall Spencer <randallspencer@gmail.com>
2017-03-22 00:12:44 -0700

    Pass GET /users/:user_id test

--- api/controllers/clients.js
+++ api/controllers/clients.js
@@ -10,13 +10,13 @@ module.exports = {
 };
 
 function addClient(req, res) {
-  res.set('Content-Type', 'application/json');
-  res.json({});
+    res.set('Content-Type', 'application/json');
+    res.json({});
 }
 
 function getClient(req, res) {
-  res.set('Content-Type', 'application/json');
-  res.json({});
+    res.set('Content-Type', 'application/json');
+    res.json({});
 }
 
 function getClients(req, res) {
@@ -26,8 +26,8 @@ function getClients(req, res) {
     // promises.push(knex("clients").select("id"));
     promises.push(knex("clients").select("id", "first_name", "last_name", "email"));
     promises.push(knex("client_recipes")
-            .join('recipes', 'recipes.id', '=', 'client_recipes.recipe_id')
-            .select("client_recipes.client_id", "recipes.*"));
+        .join('recipes', 'recipes.id', '=', 'client_recipes.recipe_id')
+        .select("client_recipes.client_id", "recipes.*"));
     Promise.all(promises)
         .then((results) => {
             let clients = results[0];
@@ -38,15 +38,52 @@ function getClients(req, res) {
                     return recipe.client_id === client.id;
                 }).map((recipe) => {
                     return {
-                      id: recipe.id,
-                      instructions: recipe.instructions,
-                      name: recipe.name
+                        id: recipe.id,
+                        instructions: recipe.instructions,
+                        name: recipe.name
                     };
                 }).sort();
 
                 client.recipes = r;
             };
 
-            return res.json({clients: clients});
+            return res.json({
+                clients: clients
+            });
+        });
+}
+
+function getClient(req, res) {
+    // To list clients
+
+    let promises = [];
+    // promises.push(knex("clients").select("id"));
+    promises.push(
+        knex("clients")
+        .select("id", "first_name", "last_name", "email")
+        .first()
+        .where("id", req.swagger.params.user_id.value)
+    );
+    promises.push(
+        knex("client_recipes")
+        .join('recipes', 'recipes.id', '=', 'client_recipes.recipe_id')
+        .select("client_recipes.client_id", "recipes.*")
+        .where("client_recipes.client_id", req.swagger.params.user_id.value)
+    );
+
+    Promise.all(promises)
+        .then((results) => {
+            let client = results[0];
+            let recipes = results[1];
+
+            client["recipes"] = recipes.map((recipe) => {
+                return {
+                    id: recipe.id,
+                    name: recipe.name,
+                    instructions: recipe.instructions
+                };
+            }).sort();
+
+            return res.json(client);
         });
 }
