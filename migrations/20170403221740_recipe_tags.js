2b47e53302138d8c42a1175b1c8916aace632d8d
Randall Spencer <randallspencer@gmail.com>
2017-04-04 16:03:42 -0700

    Try again to fix heroku deploy

---
+++ migrations/20170403221740_recipe_tags.js
@@ -0,0 +1,11 @@
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('recipe_tags', (table) => {
+        table.increments('id').primary();
+        table.integer('recipe_id').references('recipes.id').onDelete('CASCADE').notNullable();
+        table.string('tag_text').notNullable().defaultTo('');
+    });
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists("recipe_tags");
+};
