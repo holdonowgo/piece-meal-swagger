8ee766c421f30494f6b891ba9235ef0b7d174503
Robert Murray <work@robertmurray.me>
2017-04-24 07:33:35 -0700

    Please add more comments to your code. You should have short comments above each function in your controllers describing what they do. Also please take out any extraneous code you're not using. There is a lot of code that you're not using that was commented out. I moved your   Usage: knex [options] [command]
      Commands:
    
        init [options]                         Create a fresh knexfile.
        migrate:make [options] <name>          Create a named migration file.
        migrate:latest                         Run all migrations that have not yet been run.
        migrate:rollback                       Rollback the last set of migrations performed.
        migrate:currentVersion                 View the current version for the migration.
        seed:make [options] <name>             Create a named seed file.
        seed:run                               Run seed files.
    
      Options:
    
        -h, --help         output usage information
        -V, --version      output the version number
        --debug            Run with debugging.
        --knexfile [path]  Specify the knexfile path.
        --cwd [path]       Specify the working directory.
        --env [name]       environment, default: process.env.NODE_ENV || development, , and  files into a  directory and made the related changes. You could also move your  directory into the same folder as well to keep things organized.

--- seeds/04_ingredient_alternatives.js
+++ seeds/04_ingredient_alternatives.js
@@ -1,58 +1,32 @@
 const data = [{
-    ingredient_id: 1,
-    alt_ingredient_id: 2
+  ingredient_id: 1,
+  alt_ingredient_id: 2
 }, {
-    ingredient_id: 1,
-    alt_ingredient_id: 3
+  ingredient_id: 1,
+  alt_ingredient_id: 3
 }, {
-    ingredient_id: 3,
-    alt_ingredient_id: 5
+  ingredient_id: 3,
+  alt_ingredient_id: 5
 }, {
-    ingredient_id: 3,
-    alt_ingredient_id: 6
+  ingredient_id: 3,
+  alt_ingredient_id: 6
 }, {
-    ingredient_id: 18,
-    alt_ingredient_id: 17
+  ingredient_id: 18,
+  alt_ingredient_id: 17
+},
+{
+  ingredient_id: 25,
+  alt_ingredient_id: 27
+},
+{
+  ingredient_id: 26,
+  alt_ingredient_id: 27
 }
-, {
-    ingredient_id: 25,
-    alt_ingredient_id: 27
-}
-, {
-    ingredient_id: 26,
-    alt_ingredient_id: 27
-}
-// , {
-//     id: 0,
-//     ingredient_id: 0,
-//     alt_ingredient_id: 0
-// }
-// , {
-//     id: 0,
-//     ingredient_id: 0,
-//     alt_ingredient_id: 0
-// }
-// , {
-//     id: 0,
-//     ingredient_id: 0,
-//     alt_ingredient_id: 0
-// }
-// ,
-// {
-//     id: 0,
-//     ingredient_id: 0,
-//     alt_ingredient_id: 0
-// }
-]
-exports.seed = function(knex, Promise) {
+];
+exports.seed = function (knex, Promise) {
   // Deletes ALL existing entries
-  return knex('ingredient_alternatives').del()
-      .then(function() {
-          //then insert the data
-          return knex('ingredient_alternatives').insert(data);
-      });
-      // .then(function() {
-      //     //update id to the maximum id using raw SQL
-      //     return knex.raw("SELECT setval('ingredient_alternatives_id_seq', (SELECT MAX(id) from ingredient_alternatives));");
-      // });
+  return knex("ingredient_alternatives").del()
+      .then(() =>
+          // then insert the data
+         knex("ingredient_alternatives").insert(data));
 };
