ae3c63c4ecd34ec4ad1779c2c009b9cbd42daefb
Randall Spencer <randallspencer@gmail.com>
2017-04-30 18:31:20 -0700

    Add ability to favorite recipes

---
+++ migrations/20170430165721_recipe_favorites.js
@@ -0,0 +1,12 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('recipe_favorites', (table) => {
+        table.increments('id').primary();
+        table.integer('recipe_id').references('recipes.id').onDelete('CASCADE').notNullable();
+        table.integer('client_id').references('clients.id').onDelete('CASCADE').notNullable();
+    });
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists("recipe_favorites");
+};
